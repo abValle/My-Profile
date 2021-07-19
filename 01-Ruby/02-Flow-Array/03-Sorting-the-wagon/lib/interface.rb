81e23f132bf24e0cf77d107e4a60b7c14c4be6be
Allan Valle <allanvalle@outlook.com>
2021-07-19 18:23:42 -0300

    Ativ 4

--- 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
@@ -6,6 +6,23 @@ require_relative "wagon_sort"
 #       - `chomp`: http://www.ruby-doc.org/core-2.5.3/String.html#method-i-chomp
 
 
+students = []
+name = nil
+
+while name != ""
+  puts "Adicione o nome: "
+  name = gets.chomp.to_s
+  next if name == ""
+
+  students << name
+  sortered_students = wagon_sort(students)
+  puts "#{students.count} student"
+  puts students[0]
+  if sortered_students.count == 3 || sortered_students.count == 4
+    puts "#{students.count} students"
+    puts "#{sortered_students[0..-2].join(', ')} and #{sortered_students.last}."
+  end
+end
 
 # TODO: Then call `wagon_sort` method defined in the wagon_sort.rb
 #       file and display the sorted student list
