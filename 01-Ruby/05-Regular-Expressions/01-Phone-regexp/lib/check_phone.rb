663f92966c30bff406e70c68e223a13d1a8957d1
Allan Valle <allanvalle@outlook.com>
2021-07-27 20:42:44 -0300

    Regex

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,8 @@
 def french_phone_number?(phone_number)
   # TODO: true or false?
+  if phone_number =~ /^(\+33)\s[1-9]\s\d{2}\s\d{2}\s\d{2}|^0[1-9](\s?|-)\d{2}(\s?|-)\d{2}(\s?|-)\d{2}(\s?|-)\d{2}/
+    return true
+  else
+    return false
+  end
 end
