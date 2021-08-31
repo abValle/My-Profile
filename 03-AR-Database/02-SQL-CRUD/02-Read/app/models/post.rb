7b342e7be51c21f3619e12dcc786a660127c6742
Allan Valle <allanvalle@outlook.com>
2021-08-31 20:09:45 -0300

    Read

--- 03-AR-Database/02-SQL-CRUD/02-Read/app/models/post.rb
+++ 03-AR-Database/02-SQL-CRUD/02-Read/app/models/post.rb
@@ -1,7 +1,45 @@
-# You can use a global variable, DB, which
-# is an instance of SQLite3::Database
-# NO NEED TO CREATE IT, JUST USE IT.
-
 class Post
-  # TODO
+  attr_reader :id
+  attr_accessor :title, :url, :votes
+
+  def initialize(attributes = {})
+    @id = attributes[:id]
+    @url = attributes[:url]
+    @votes = attributes[:votes] || 0
+    @title = attributes[:title]
+  end
+
+  def self.find(id)
+    # TRANSFORMAR RESULTADO TRAGO EM ARRAY DE HASHS
+    DB.results_as_hash = true
+    # PESQUISANDO NO BANCO E SALVANDO EM UMA VARIAVEL. UTILIZAR ? PARA EVIDAR SQL INJECTIONS
+    # .first PARA PEGAR A HASH(RETIRANDO DA ARRAY)
+    first_row = DB.execute("SELECT * FROM posts WHERE id = ?", id).first
+    # PASSANDO O RESULTADO CASO EXISTA(IF) PARA O METODO build_record, PARA QUE SEJA INSTANCIADO O OBJETO
+    build_record(first_row) if first_row
+  end
+
+  def self.all
+    # TRANSFORMAR RESULTADO TRAGO EM ARRAY DE HASHS
+    DB.results_as_hash = true
+    # SELECIONANDO TODOS OS REGISTROS DE posts.
+    # PASSANDO TODOS OS REGISTROS PELO map PARA QUE SEJA ALTERADA A HASH E INSTANCIADO OS OBJETOS
+    DB.execute("SELECT * FROM posts").map { |row| build_record(row) }
+  end
+
+  def self.build_record(row)
+    # CRIANDO A HASH PARA QUE SEJA INSTANCIADO O OBJETO
+    # ROW E' UMA HASH PROVENIENTE DO SELECT
+    attributes = {
+      # id: recebe o VALOR de row["id"]
+      id: row["id"],
+      # url: recebe o VALOR de row["url"]
+      url: row["url"],
+      # etc
+      votes: row["votes"],
+      title: row["title"]
+    }
+    # INSTANCIANDO O OBJETO
+    Post.new(attributes)
+  end
 end
--- 03-AR-Database/02-SQL-CRUD/02-Read/test.rb
+++ 03-AR-Database/02-SQL-CRUD/02-Read/test.rb
@@ -1,10 +1,13 @@
 require_relative "app/models/post"
 require "sqlite3"
 
+# CRIANDO O BANCO
 db_file_path = File.join(File.dirname(__FILE__), "spec/support/posts_spec.db")
 DB = SQLite3::Database.new(db_file_path)
 
+# DELETANDO O BANCO PARA SOBRESCREVER AO EXECUTAR O PROGRAMA
 DB.execute('DROP TABLE IF EXISTS `posts`;')
+# DEFININDO O SQL
 create_statement = "
     CREATE TABLE `posts` (
       `id`  INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -12,7 +15,9 @@ create_statement = "
       `url` TEXT,
       `votes`  INTEGER
     );"
+# CRIANDO O BANDO
 DB.execute(create_statement)
+# ADICIONANDO NOVOS SITES
 DB.execute("INSERT INTO `posts` (title, url, votes) VALUES ('Le Wagon', 'www.lewagon.com', '9000')")
 DB.execute("INSERT INTO `posts` (title, url, votes) VALUES ('Github', 'www.github.com', '1600')")
 DB.execute("INSERT INTO `posts` (title, url, votes) VALUES ('Slack', 'www.slack.com', '4000')")
@@ -20,3 +25,8 @@ DB.execute("INSERT INTO `posts` (title, url, votes) VALUES ('Mozilla', 'www.mozi
 DB.execute("INSERT INTO `posts` (title, url, votes) VALUES ('Stackoverflow', 'www.stackoverflow.com', '4300')")
 
 # Write your test code here (and run `ruby test.rb` in your terminal to run it):
+
+sites = Post.all
+sites.each do |site|
+  puts "#{site.id}. #{site.title} -> VOTE[#{site.votes}] SITE: #{site.url}"
+end
