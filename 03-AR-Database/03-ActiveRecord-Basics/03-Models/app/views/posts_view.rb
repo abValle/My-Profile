cd976849f3f948cc425e6831af419e48ca8f4304
Allan Valle <allanvalle@outlook.com>
2021-09-04 11:45:15 -0300

    Models

--- 03-AR-Database/03-ActiveRecord-Basics/03-Models/app/views/posts_view.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/03-Models/app/views/posts_view.rb
@@ -1,3 +1,13 @@
 class PostsView
   # TODO: implement some methods here when controller needs to `puts` or `gets`
+  def display(posts)
+    posts.each do |post|
+      puts "#{post.id} - #{post.title} - #{post.url} - #{post.votes}"
+    end
+  end
+
+  def ask_user_for(something)
+    puts something.to_s
+    gets.chomp
+  end
 end
